8bc58264de9c0cc3b33ee09cc749882b356d3a8f
Voodoo Huang <voodoo0406@gmail.com>
2020-10-05 16:05:23 +0800

    Try default binder ApplicationContext

--- app/src/main/java/tw/com/voodoo0406/tryhilt/Apple.java
+++ app/src/main/java/tw/com/voodoo0406/tryhilt/Apple.java
@@ -1,12 +1,22 @@
 package tw.com.voodoo0406.tryhilt;
 
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+import dagger.hilt.android.qualifiers.ApplicationContext;
 import javax.inject.Inject;
 
 public class Apple extends Fruit {
 
   @Inject
-  public Apple(Origin origin) {
+  public Apple(@ApplicationContext Context applicationContext, Application application,
+      Origin origin) {
     super(origin);
+    Log.d("GGGGG",
+        "applicationContext is " + (applicationContext == null ? "null" : "NOT null") + ", "
+            + applicationContext);
+    Log.d("GGGGG",
+        "application is " + (application == null ? "null" : "NOT null") + ", " + application);
   }
 
   @Override
